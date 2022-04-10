fda6eb2c366e9b5adf90b3f91c341bbe4741b44d
abii19 <joshiabii19@gmail.com>
2022-04-10 18:18:42 +0545

    Navbar and Banner Section Designed

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
 import React from "react";
-// import { createRoot } from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import "./assets/css/index.css";
 import App from "./App";
 
-import ReactDOM from "react-dom";
-
-// const container = document.getElementById("root");
-// const root = createRoot(container);
-// root.render(<App />);
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root"),
-);
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
